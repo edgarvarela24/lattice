016c5590ed645b530d34d7e5a0c1bf898adbd657
Edgar Varela <edgarvarela24@gmail.com>
2026-03-08 13:51:02 -0700

    garbage collection tests/verification

--- packages/signals/vitest.config.ts
+++ packages/signals/vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    pool: 'forks', // --expose-gc doesn't work with threads
+    poolOptions: {
+      forks: {
+        execArgv: ['--expose-gc'],
+      },
+    },
     include: ['__tests__/**/*.test.ts'],
     globals: true,
     environment: 'node',
